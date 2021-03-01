e0998f3f4b86e122739f985e556e36e03920f3d5
PresentJay <presentj94@gmail.com>
2021-03-01 11:27:00 +0900

    fastapi tutorial from doc : path parameters

--- test/03_fastapi-from-doc[processing]/main.py
+++ test/03_fastapi-from-doc[processing]/main.py
@@ -8,6 +8,9 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+fake_items_db = [ {"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
@@ -23,9 +26,15 @@ class Item(BaseModel):
 def read_root():
     return {'Hello': 'World'}
 
+@app.get("/items/")
+async def read_item(skip: int=0, limit: int=10):
+    return fake_items_db[skip : skip + limit]
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: str, q: Optional[str] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item:Item):
