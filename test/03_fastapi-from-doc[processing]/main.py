3798be70bf4ee8f35a795bb1d9eaf1a08c6a2c24
PresentJay <presentj94@gmail.com>
2021-03-01 11:20:54 +0900

    fastapi tutorial from doc : enumeration

--- test/03_fastapi-from-doc[processing]/main.py
+++ test/03_fastapi-from-doc[processing]/main.py
@@ -1,14 +1,23 @@
 from typing import Optional
-from fastapi import FastAPI
+from enum import Enum
+# Enumarations are available in python since version 3.4
 
+
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Optional[bool] = None
+
 
 @app.get("/")
 def read_root():
@@ -21,3 +30,21 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item:Item):
     return {"item_name": item.name, "item_id": item.id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id" : "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deel Learning FTW!"} # For The Win!
+    
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuals"} # residual - 잔차
