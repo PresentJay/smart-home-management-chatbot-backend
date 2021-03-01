6c180399ff2e925449fcb1f5968cdb5f9ece5896
PresentJay <presentj94@gmail.com>
2021-03-01 15:19:08 +0900

    fastapi tutorial from doc : use the model

--- test/03_fastapi-from-doc[processing]/request/main.py
+++ test/03_fastapi-from-doc[processing]/request/main.py
@@ -12,6 +12,10 @@ class Item(BaseModel):
     
 app = FastAPI()
 
-@app.get("/items/")
+@app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
