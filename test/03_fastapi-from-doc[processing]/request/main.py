90ba541bb4932cc032316ea8737d075c6e829f51
PresentJay <presentj94@gmail.com>
2021-03-02 23:09:25 +0900

    fastapi - heroku automatic deploy test

--- test/03_fastapi-from-doc[processing]/request/main.py
+++ test/03_fastapi-from-doc[processing]/request/main.py
@@ -25,4 +25,4 @@ async def create_item(item_id: int, item: Item, q:Optional[str] = None):
     result = {"item_id": item_id, **item.dict()}
     if q:
         result.update({"q": q})
-    return result
+    return result 
