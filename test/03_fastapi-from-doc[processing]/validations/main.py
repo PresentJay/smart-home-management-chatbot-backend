9ab6603931bc13e0117c365585b5cb0bc958c7a9
PresentJay <presentj94@gmail.com>
2021-03-03 23:33:31 +0900

    fastapi tutorial from doc : make deprecated state

--- test/03_fastapi-from-doc[processing]/validations/main.py
+++ test/03_fastapi-from-doc[processing]/validations/main.py
@@ -8,7 +8,8 @@ async def read_items(q: Optional[str] = Query(
     ...,
     title= "Query String",
     description= "Query string for the items to search in the database that have a good match",
-    min_length=3, max_length=50, regex="^fixedquery$"
+    min_length=3, max_length=50, regex="^fixedquery$",
+    deprecated=True
     )):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
