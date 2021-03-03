dff8b32eb4a6e30fe50769f3735285aeca54f4fa
PresentJay <presentj94@gmail.com>
2021-03-03 23:12:18 +0900

    fastapi tutorial from doc : how to declare it is required in Query function

--- test/03_fastapi-from-doc[processing]/validations/main.py
+++ test/03_fastapi-from-doc[processing]/validations/main.py
@@ -4,11 +4,14 @@ from fastapi import FastAPI, Query
 app = FastAPI()
 
 @app.get("/items/")
-async def read_items(q: Optional[str] = Query(None,min_length=3, max_length=50)):
+async def read_items(q: Optional[str] = Query(
+    ...,min_length=3, max_length=50, regex="^fixedquery$"
+    )):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
     return results
 
 # q is not required
-# set the max length
+# can set the min or max length
+# also can set the regular expression
