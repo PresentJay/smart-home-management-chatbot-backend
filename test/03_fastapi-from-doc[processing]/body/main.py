f3e8179f42bd0fcd13090d8875d13ef205b64b76
PresentJay <presentj94@gmail.com>
2021-03-04 23:53:13 +0900

    fastapi tutorial from doc : multiple body params and query

--- test/03_fastapi-from-doc[processing]/body/main.py
+++ test/03_fastapi-from-doc[processing]/body/main.py
@@ -11,16 +11,20 @@ class Item(BaseModel):
     price: float
     tax: Optional[float] = None
     
-
+class User(BaseModel):
+    username: str
+    full_name: Optional[str] = None
+    
+@app.put("/items/{item_id}")
 async def update_item(
     *,
-    item_id: int = Path(..., title="The ID of the item to get", ge=0, le=1000),
-    q: Optional[str] = None,
-    item: Optional[Item] = None,    
+    item_id:int,
+    item: Item,
+    user: User,
+    importance: int = Body(..., gt=0), # required
+    q: Optional[str] = None
 ):
-    results = {"item_id" : item_id}
+    results = {"item_id" : item_id, "item": item, "user": user, "importance": importance}
     if q:
         results.update({"q": q})
-    if item:
-        results.update({"item": item})
     return results
